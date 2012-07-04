2ea056e1d06c92acc0a13a4ddc51fecd4814d729
Андрей Малышев <andrei.malyshev@kodep.ru>
2012-07-04 21:29:28 +0400

    (#1226) Context menu hook for issues to bulk assign them to milestone

--- lib/milestones/hooks.rb
+++ lib/milestones/hooks.rb
@@ -4,5 +4,6 @@ module Milestones
     render_on :view_projects_roadmap_version_bottom, :partial => 'hooks/milestones/view_projects_roadmap_version_bottom'
     render_on :view_issues_form_details_bottom, :partial => 'hooks/milestones/view_issues_form_details_bottom'
     render_on :view_issues_show_details_bottom, :partial => 'hooks/milestones/view_issues_show_details_bottom'
+    render_on :view_issues_context_menu_start, :partial => 'hooks/milestones/view_issues_context_menu_start'
   end
 end
